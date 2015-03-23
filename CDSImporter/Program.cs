cc156618f2a8e376e681dbe4b5ef98115878e794
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2015-03-23 17:22:31 +0530

    csv to sqlite

---
+++ CDSImporter/Program.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using System.IO;
+
+namespace CDSImporter
+{
+	class CDSImporter
+	{
+		public static void Main (string[] args)
+		{
+			var csv = new CsvReader (File.OpenText (args [0]));
+			csv.Read ();
+			var sqliteTable = new SqliteTable(Path.GetFileNameWithoutExtension(args [0]), csv.FieldHeaders);
+			do {
+				sqliteTable.InsertData(csv.CurrentRecord);
+			} while (csv.Read ());
+		}
+	}
+}
---
+++ CDSImporter/SqliteTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace CDSImporter
+{
+	public class SqliteTable
+	{
+		private string[] columns;
+
+		private SQLiteConnection dbConnection;
+
+		public SqliteTable (string name, string[] columns)
+		{
+			this.columns = columns;
+			SQLiteConnection.CreateFile (String.Format ("{0}.sqlite", name));
+			dbConnection = new SQLiteConnection (String.Format ("Data Source={0}.sqlite;Version=3;", name));
+		}
+
+		public bool Create ()
+		{
+			dbConnection.Open ();
+			string sql = String.Format ("create table dataset ({0})", allColumns ());
+			new SQLiteCommand (sql, dbConnection).ExecuteNonQuery ();
+			dbConnection.Close ();
+			return true;
+		}
+
+		public bool InsertData (string[] data)
+		{
+			dbConnection.Open ();
+			string sql = String.Format ("insert into dataset ({0}) values ({1})", allColumns (), String.Join (",", data));
+			new SQLiteCommand (sql, dbConnection).ExecuteNonQuery ();
+			dbConnection.Close ();
+			return true;
+		}
+
+		string allColumns ()
+		{
+			return String.Join (" TEXT,", this.columns);
+		}
+	}
+}
+
